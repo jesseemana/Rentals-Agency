ba844ab962c7bcb7e90296768f68e80d20ff1504
Jesse Emana <jesseemana@gmail.com>
2023-01-15 22:22:45 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="">
+      <h1 className="text-red-500 uppercase">Rentals Agency Website</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Flexibility.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Flexibility = () => {
+  return <div>Flexibility</div>;
+};
